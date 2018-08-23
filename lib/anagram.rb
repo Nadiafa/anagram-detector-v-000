21e4a482a10bdc984b72df94bab71a833ad13a6b
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-23 10:23:24 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,16 @@
-class  
+class Anagram
   
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end 
 
+  def match(array)
+    result = nil 
+    array.
+    
+  end 
 
 
 end 
